83082c61d4f28da06b13ab9acb2f566b82823669
G7-Hckr <alfitopasha213@gmail.com>
2019-03-23 12:07:24 +0700

    project

---
+++ module/load.sh
@@ -0,0 +1,201 @@
+clear
+echo "[Loading 1%]"
+clear
+echo "[Loading 2%]"
+clear
+echo "[Loading 3%]"
+clear
+echo "[Loading 4%]"
+clear
+echo "[Loading 5%]"
+clear
+echo "[Loading 6%]"
+clear
+echo "[Loading 7%]"
+clear
+echo "[Loading 8%]"
+clear
+echo "[Loading 9%]"
+clear
+echo "[Loading 10%]"
+clear
+echo "[Loading 11%]"
+clear
+echo "[Loading 12%"
+clear
+echo "[Loading 13%]"
+clear
+echo "[Loading 14%]"
+clear
+echo "[Loading 15%]"
+clear
+echo "[Loading 16%]"
+clear
+echo "[Loading 17%]"
+clear
+echo "[Loading 18%]"
+clear
+echo "[Loading 19%]"
+clear
+echo "[Loading 20%]"
+clear
+echo "[Loading 21%]"
+clear
+echo "[Loading 22%]"
+clear
+echo "[Loading 23%]"
+clear
+echo "[Loading 24%]"
+clear
+echo "[Loading 25%]"
+clear
+echo "[Loading 26%]"
+clear
+echo "[Loading 27%]"
+clear
+echo "[Loading 28%]"
+clear
+echo "[Loading 29%]"
+clear
+echo "[Loading 30%]"
+clear
+echo "[Loading 31%]"
+clear
+echo "[Loading 32%]"
+clear
+echo "[Loading 33%]"
+clear
+echo "[Loading 34%]"
+clear
+echo "[Loading 35%]"
+clear
+echo "[Loading 36%]"
+clear
+echo "[Loading 37%]"
+clear
+echo "[Loading 38%]"
+clear
+echo "[Loading 39%]"
+clear
+echo "[Loading 40%]"
+clear
+echo "[Loading 41%]"
+clear
+echo "[Loading 42%]"
+clear
+echo "[Loading 43%]"
+clear
+echo "[Loading 44%]"
+clear
+echo "[Loading 45%]"
+clear
+echo "[Loading 46%]"
+clear
+echo "[Loading 47%]"
+clear
+echo "[Loading 48%]"
+clear
+echo "[Loading 49%]"
+clear
+echo "[Loading 50%]"
+clear
+echo "[Loading 51%]"
+clear
+echo "[Loading 52%]"
+clear
+echo "[Loading 53%]"
+clear
+echo "[Loading 54%]"
+clear
+echo "[Loading 55%]"
+clear
+echo "[Loading 56%]"
+clear
+echo "[Loading 57%]"
+clear
+echo "[Loading 58%]"
+clear
+echo "[Loading 59%]"
+clear
+echo "[Loading 60%]"
+clear
+echo "[Loading 61%]"
+clear
+echo "[Loading 62%]"
+clear
+echo "[Loading 63%]"
+clear
+echo "[Loading 64%]"
+clear
+echo "[Loading 65%]"
+clear
+echo "[Loading 66%]"
+clear
+echo "[Loading 67%]"
+clear
+echo "[Loading 68%]"
+clear
+echo "[Loading 69%]"
+clear
+echo "[Loading 70%]"
+clear
+echo "[Loading 71%]"
+clear
+echo "[Loading 72%]"
+clear
+echo "[Loading 73%]"
+clear
+echo "[Loading 74%]"
+clear
+echo "[Loading 75%]"
+clear
+echo "[Loading 76%]"
+clear
+echo "[Loading 77%]"
+clear
+echo "[Loading 78%]"
+clear
+echo "[Loading 79%]"
+clear
+echo "[Loading 80%]"
+clear
+echo "[Loading 81%]"
+clear
+echo "[Loading 82%]"
+clear
+echo "[Loading 83%]"
+clear
+echo "[Loading 84%]"
+clear
+echo "[Loading 85%]"
+clear
+echo "[Loading 86%]"
+clear
+echo "[Loading 87%]"
+clear
+echo "[Loading 88%]"
+clear
+echo "[Loading 89%]"
+clear
+echo "[Loading 90%]"
+clear
+echo "[Loading 91%]"
+clear
+echo "[Loading 92%]"
+clear
+echo "[Loading 93%]"
+clear
+echo "[Loading 94%]"
+clear
+echo "[Loading 95%]"
+clear
+echo "[Loading 96%]"
+clear
+echo "[Loading 97%]"
+clear
+echo "[Loading 98%]"
+clear
+echo "[Loading 99%]"
+clear
+echo "[Loading 100%]"
+clear
